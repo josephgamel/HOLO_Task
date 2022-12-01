92f455e4f6e993afcf9c32314cd6f561ce9398dd
joseph.gamil <joseph.gamil@itworx.com>
2022-12-01 19:05:56 +0200

    first commit

---
+++ src/main/java/Pages/mortgagePage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MortagePage {
+    private WebDriver driver ;
+    public MortagePage(WebDriver driver){
+        this.driver = driver ;
+    }
+    public void selectMortageOptions(){
+
+    }
+}
---
+++ src/test/java/Mortgage/MortgageTests.java
@@ -0,0 +1,2 @@
+package Mortgage;public class MortgageTests {
+}
